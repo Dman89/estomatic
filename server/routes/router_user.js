8cbf606c317b31c2df451c2a05d93b69753fd277
Daniel Cudney <upnvapesd@gmail.com>
2017-02-25 15:08:58 -0800

    Test DB

--- server/routes/router_user.js
+++ server/routes/router_user.js
@@ -5,6 +5,9 @@ const passport = require('passport');
 const requireAuth = passport.authenticate('jwt', {session: false});
 const requireLogin = passport.authenticate('local', {session: false});
 module.exports = function(app) {
+  app.get('/api/welcome', function(req, res) {
+    res.status(200).send({welcome: "Welcome"});
+  })
   app.get('/api/profile', requireAuth, function(req, res) {
     res.status(200).send({user: req.user});
   })
