a64fbf9d39a97417e9e9b86655c1dd99a60400bd
Dman89 <upnvapesd@gmail.com>
2017-02-26 16:06:01 -0800

    Changed DB Url

--- server/database.js
+++ server/database.js
@@ -4,7 +4,7 @@ var user = process.env.DB || config.user;
 var password = process.env.DB || config.password;
 var DB = process.env.DB || 'mongodb://'+user+':'+password+'@ds161209.mlab.com:61209/ncc-1701-d';
 var TESTDB = "mongodb://localhost:27017/test";
-mongoose.connect(TESTDB, function(err) {
+mongoose.connect(DB, function(err) {
   if (err) {
     console.log("Error Connecting");
     console.log(err);
